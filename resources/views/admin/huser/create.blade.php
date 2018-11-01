993aaf4c41521003c8797229f3cdbc7d9876f090
DFSir <15378596555@163.com>
2018-11-01 21:18:16 +0800

    第一次合并其他两个分支后上传

--- resources/views/admin/huser/create.blade.php
+++ resources/views/admin/huser/create.blade.php
@@ -22,37 +22,69 @@
 			{{ csrf_field() }}
 			
 				<div class="row cl">
-					<label class="form-label col-xs-4 col-sm-3"><span class="c-red">*</span>用户名：</label>
+					<label class="form-label col-xs-4 col-sm-1"><span class="c-red">*</span>用户名：</label>
 					<div class="formControls col-xs-4 col-sm-4">
-						<input type="text" class="input-text" value="" placeholder="" id="uname" name="uname"><span></span>
+						<input type="text" class="input-text" value="" id="uname" name="uname">
+					</div>
+					<div class="col-xs-4 col-ms-4 spanll">
 						
 					</div>
 				</div>
 				<div class="row cl">
-					<label class="form-label col-xs-4 col-sm-3"><span class="c-red">*</span>账号：</label>
+					<label class="form-label col-xs-4 col-sm-1"><span class="c-red">*</span>账号：</label>
 					<div class="formControls col-xs-4 col-sm-4">
-						<input type="text" class="input-text" value="" placeholder="" id="uaccnum" name="uaccnum">
+						<input type="text" class="input-text" value="" id="uaccnum" name="uaccnum">
+					</div>
+					<div class="col-xs-4 col-ms-4 spanll">
+						
 					</div>
 				</div>
 				
 				<div class="row cl">
-					<label class="form-label col-xs-4 col-sm-3"><span class="c-red">*</span>密码：</label>
+					<label class="form-label col-xs-4 col-sm-1"><span class="c-red">*</span>密码：</label>
 					<div class="formControls col-xs-4 col-sm-4">
-						<input type="password" class="input-text" value="" placeholder="" id="upasswd" name="upasswd">
+						<input type="password" class="input-text" value="" id="upasswd" name="upasswd">
+					</div>
+					<div class="col-xs-4 col-ms-4 spanll">
+						
 					</div>
 				</div>
 				
 				
 				
 				<div class="row cl">
-					<div class="col-xs-4 col-sm-4 col-xs-offset-4 col-sm-offset-3">
+					<div class="col-xs-4 col-sm-2  col-sm-offset-1">
 						<input class="btn btn-primary radius" type="submit" value="&nbsp;&nbsp;添加&nbsp;&nbsp;">
 					</div>
 				</div>
 			
 		</form>
 	</article>
+	
+	<script type="text/javascript">
+		var isUser = false;
+		$('#uname').focus(function(){
+			$('.spanll').eq(0).html('<font color="#ccc">请输入4-12位数字字母下划线组合,字母开头</font>');
+		}).blur(function(){
+			var user_preg = /^[a-zA-Z]{1}[\w]{7,16}$/;
+            var user_vals = $('#uname').val();
+            if(user_preg.test(user_vals)){
+                isUser = true;
+                $('.spanll').eq(0).html('<font color="#0f0">用户名格式正确</font>');
+            }else{
+                isUser = false;
+                $('.spanll').eq(0).html('<font color="#f00">用户名格式错误</font>');
+            }
+		});
 
+
+		$('form').submit(function(){
+            if(isUser){
+                return true;
+            }
+            return false;
+        });
+	</script>
 
 
 @endsection
